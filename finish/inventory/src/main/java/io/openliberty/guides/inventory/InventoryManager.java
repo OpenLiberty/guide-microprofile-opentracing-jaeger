7320c07d9e3fa15a6bf2423be523a2cdb9dd82e4
Priyanshi Patel <priyanshi.patel@ibm.com>
2021-05-12 10:38:28 -0400

    Update InventoryManager.java

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -1,15 +1,15 @@
-//tag::copyright[]
+// tag::copyright[]
 /*******************************************************************************
-* Copyright (c) 2017, 2021 IBM Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     IBM Corporation - Initial implementation
-*******************************************************************************/
-//end::copyright[]
+ * Copyright (c) 2017, 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
 package io.openliberty.guides.inventory;
 
 import java.util.ArrayList;
@@ -25,8 +25,7 @@ import org.eclipse.microprofile.opentracing.Traced;
 
 import io.opentracing.Scope;
 import io.opentracing.Tracer;
-import io.opentracing.Tracer.SpanBuilder;
-import io.openliberty.guides.inventory.model.*;
+import io.openliberty.guides.inventory.model;
 
 @ApplicationScoped
 // tag::InventoryManager[]
