63cf4bd7ecc7d71521ba91534ea0379b5a6a4fab
Gilbert Kwan <gkwan@ca.ibm.com>
2021-05-20 17:27:31 -0400

    Version update (#38)
    * update versions
    
    * Update testApp.sh
    
    * Update InventoryManager.java
    
    * Update InventoryManager.java
    
    * Update dailyBuild.sh
    
    * Update InventoryManager.java
    
    * Update InventoryManager.java
    
    * Update InventoryManager.java
    
    * Update InventoryManager.java
    
    * Update pom.xml
    
    * Update testApp.sh
    
    Co-authored-by: Priyanshi Patel <priyanshi.patel@ibm.com>

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryManager.java
@@ -1,36 +1,36 @@
-//tag::copyright[]
+// tag::copyright[]
 /*******************************************************************************
-* Copyright (c) 2017, 2020 IBM Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-*     IBM Corporation - Initial implementation
-*******************************************************************************/
-//end::copyright[]
+ * Copyright (c) 2017, 2021 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - Initial implementation
+ *******************************************************************************/
+// end::copyright[]
 package io.openliberty.guides.inventory;
 
 import java.util.ArrayList;
 import java.util.Properties;
 import io.openliberty.guides.inventory.client.SystemClient;
 import io.openliberty.guides.inventory.model.InventoryList;
+import io.openliberty.guides.inventory.model.SystemData;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
 import java.util.Collections;
 
 import org.eclipse.microprofile.opentracing.Traced;
-
 import io.opentracing.Scope;
 import io.opentracing.Tracer;
-import io.openliberty.guides.inventory.model.*;
+import io.opentracing.Span;
 
 @ApplicationScoped
 // tag::InventoryManager[]
 public class InventoryManager {
-    
+
     private List<SystemData> systems = Collections.synchronizedList(new ArrayList<>());
     private SystemClient systemClient = new SystemClient();
     // tag::customTracer[]
@@ -40,7 +40,6 @@ public class InventoryManager {
     public Properties get(String hostname) {
         systemClient.init(hostname, 9080);
         Properties properties = systemClient.getProperties();
-        
         return properties;
     }
 
@@ -52,17 +51,15 @@ public class InventoryManager {
         SystemData system = new SystemData(hostname, props);
         // tag::Add[]
         if (!systems.contains(system)) {
-            // tag::Try[]
             // tag::addSpan[]
-            try (Scope childScope = tracer.buildSpan("add() Span")
-                                              .startActive(true)) {
+            Span span = tracer.buildSpan("add() Span").start();
             // end::addSpan[]
+            // tag::Try[]
+            try (Scope childScope = tracer.activateSpan(span)) {
                 // tag::addToInvList[]
                 systems.add(system);
                 // end::addToInvList[]
-            // tag::endTry[]
             }
-            // end::endTry[]
             // end::Try[]
         }
         // end::Add[]
--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -18,8 +18,11 @@ export JAEGER_SAMPLER_PARAM=1
 #       liberty:create            - Create a Liberty server.
 #       liberty:install-feature   - Install a feature packaged as a Subsystem Archive (esa) to the Liberty runtime.
 #       liberty:deploy            - Copy applications to the Liberty server's dropins or apps directory.
-mvn -pl system,inventory -q clean package liberty:create liberty:install-feature liberty:deploy
-
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl system,inventory -q clean package liberty:create liberty:install-feature liberty:deploy
+    
 ## Run the tests
 # These commands are separated because if one of the commands fail, the test script will fail and exit.
 # e.g if liberty:start fails, then there is no need to run the failsafe commands.
@@ -28,6 +31,9 @@ mvn -pl system,inventory -q clean package liberty:create liberty:install-feature
 #       liberty:stop              - Stop a Liberty server.
 #       failsafe:verify           - Verifies that the integration tests of an application passed.
 mvn -pl system,inventory liberty:start
-mvn -pl system,inventory failsafe:integration-test
+mvn -Dhttp.keepAlive=false \
+    -Dmaven.wagon.http.pool=false \
+    -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
+    -pl system,inventory failsafe:integration-test
 mvn -pl system,inventory liberty:stop
 mvn -pl system,inventory failsafe:verify
