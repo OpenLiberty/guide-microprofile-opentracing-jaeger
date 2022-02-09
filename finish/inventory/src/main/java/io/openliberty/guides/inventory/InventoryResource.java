71a1da43c9773e1f5d60afe9475141bc64b1a3e7
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-09 09:43:57 -0500

    star imports

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -15,10 +15,13 @@ package io.openliberty.guides.inventory;
 import java.util.Properties;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.websocket.server.PathParam;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 import org.eclipse.microprofile.opentracing.Traced;
 
 import io.openliberty.guides.inventory.model.InventoryList;
--- start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
+++ start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointIT.java
@@ -20,9 +20,16 @@ import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class InventoryEndpointIT {
