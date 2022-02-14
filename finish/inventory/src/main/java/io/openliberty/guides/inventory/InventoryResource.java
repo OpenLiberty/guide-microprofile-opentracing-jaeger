fb80a31b8203e5734b48c94ec610403e506ceeea
t27gupta <tushar.gupta@uwaterloo.ca>
2022-02-14 13:09:31 -0500

    import fix

--- finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
+++ finish/inventory/src/main/java/io/openliberty/guides/inventory/InventoryResource.java
@@ -13,18 +13,19 @@
 package io.openliberty.guides.inventory;
 
 import java.util.Properties;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
-import jakarta.websocket.server.PathParam;
-import jakarta.ws.rs.DELETE;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import jakarta.ws.rs.core.Response;
+
 import org.eclipse.microprofile.opentracing.Traced;
 
 import io.openliberty.guides.inventory.model.InventoryList;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import openj9.internal.tools.attach.target.Response;
 
 @RequestScoped
 @Path("/systems")
